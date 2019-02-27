654b02a6f4aed544a31e6d71a519c5baec2c4ec5
susanweber <scweber@stanford.edu>
2019-02-26 16:28:29 -0800

    new files, added logging

--- SupportForm.php
+++ SupportForm.php
@@ -1,12 +1,19 @@
 <?php
 namespace Stanford\SupportForm;
 
+include_once "emLoggerTrait.php";
+
 use REDCap;
+
 
 class SupportForm extends \ExternalModules\AbstractExternalModule
 {
+    use emLoggerTrait;
+
     function __construct()
     {
         parent::__construct();
+
+        $this->emDebug('hello');
     }
 }
